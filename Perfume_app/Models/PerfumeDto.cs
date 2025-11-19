0f37bd29e110c5dad740b8cf50c218c61b7b3f08
ADRIANH <rosalesadrian017@gmail.com>
2025-11-19 13:05:23 -0600

    First commit , Web description and selects created , I started to create DTos and Repositorys

---
+++ Perfume_app/Models/PerfumeDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Perfume_app.Models
+{
+    public class PerfumeDto
+    {
+        public string? Nombre { get; set; }
+        public string? Marca { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Tipo_clima  { get; set; }
+        public string? Genero { get; set; }
+        public int edad_recomendada { get; set; }
+    }
+}
---
+++ Perfume_app/Services/RepositorioPerfumes.cs
@@ -0,0 +1,33 @@
+﻿using Perfume_app.Models;
+
+namespace Perfume_app.Services
+{
+    public class RepositorioPerfumes
+    {
+        public List<PerfumeDto> ObtenerPerfumes()
+        {
+            return new List<PerfumeDto>
+            {
+                new PerfumeDto
+                {
+                    Nombre = "Euphoria",
+                    Marca = "Calvin Klein",
+                    Descripcion = "Una fragancia oriental floral que evoca misterio y sensualidad.",
+                    Tipo_clima = "Frío",
+                    edad_recomendada = 25,
+                    Genero = "Femenino"
+
+                },
+                new PerfumeDto
+                {
+                    Nombre = "Light Blue",
+                    Marca = "Dolce & Gabbana",
+                    Descripcion = "Una fragancia fresca y afrutada perfecta para el verano.",
+                    Tipo_clima = "Frío",
+                    edad_recomendada = 25,
+                    Genero = "Femenino"
+                },
+            };
+        }
+    }
+}
